31d105dbf7000b1a7e1fe207f2c127bbf3fd992a
BuckanianMark <127871026+BuckanianMark@users.noreply.github.com>
2023-12-17 15:49:16 +0300

    A meme generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {ColorChromeModule} from 'ngx-color/chrome'
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GeneratorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ColorChromeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
